a8b666e8e8c2e41d75c0d9ad78fcc3e045c5b0f0
benjaminburga <burgabenjamin872@gmail.com>
2024-01-21 17:39:06 -0500

    Add two jobs and a resume to the program.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "CSTI";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Customer Service";
+        job2._company = "Teleperformance";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Benjamin Burga";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
